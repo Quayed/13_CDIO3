621c503f40156c3bfcb9f0fb39057b7629986124
Mathias Sondrup <matsondrup@gmail.com>
2015-04-24 18:20:54 +0200

    Started to imlement click functionality,

--- src/com/cdio3/client/_3_cdio3.java
+++ src/com/cdio3/client/_3_cdio3.java
@@ -1,6 +1,9 @@
 package com.cdio3.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.RootPanel;
 
@@ -17,6 +20,13 @@ public class _3_cdio3 implements EntryPoint {
 		createOperatorAnchor.setText("Create Operator");
 		createOperatorAnchor.setHref("#");
 		
+		createOperatorAnchor.addClickHandler(new ClickHandler(){
+			public void onClick(ClickEvent event){
+				clearContent();
+				// show the fields needed to create an operator.
+			}
+		});
+		
 		Anchor viewOperatorAnchor = new Anchor();
 		viewOperatorAnchor.setText("View operators");
 		viewOperatorAnchor.setHref("#");
@@ -28,6 +38,12 @@ public class _3_cdio3 implements EntryPoint {
 		RootPanel.get("nav").add(createOperatorAnchor);
 		RootPanel.get("nav").add(viewOperatorAnchor);
 		RootPanel.get("nav").add(updateOperatorAnchor);
+		
+		
 	};
+	
+	private void clearContent(){
+		// This function should clear the content div from everything inside of it.
+	}
 
 }
