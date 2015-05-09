7bcee1e4fa52a5c15d74f0bec3453ed272c2a8ea
al <a@saloweb.dk>
2015-05-09 13:57:47 +0200

    create operator to uiBinder

--- src/com/cdio3/client/boundary/views/admin/CreateWidget.java
+++ src/com/cdio3/client/boundary/views/admin/CreateWidget.java
@@ -2,145 +2,107 @@ package com.cdio3.client.boundary.views.admin;
 
 import com.cdio3.client.service.DataServiceAsync;
 import com.cdio3.shared.DALException;
+import com.cdio3.shared.FieldVerifier;
 import com.cdio3.shared.OperatorDTO;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.FlexTable;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
-import com.cdio3.shared.FieldVerifier;
+import com.google.gwt.user.client.ui.Widget;
 
 public class CreateWidget extends Composite {
-	private VerticalPanel view;
-	private DataServiceAsync serverConn;
-	private TextBox oprNameTextBox;
-	private TextBox iniTextBox;
-	private TextBox cprTextBox;
+
+	interface CreateUiBinder extends UiBinder<Widget, CreateWidget> {}
+
+	private static CreateUiBinder uiBinder = GWT.create(CreateUiBinder.class);
+	
+	private DataServiceAsync service;
+		
+	@UiField TextBox oprNameTextBox;
+	@UiField TextBox iniTextBox;
+	@UiField TextBox cprTextBox;
+	@UiField Button submitCreateOperator;
+	
+	
 	private boolean validName;
 	private boolean validCPR;
 	private boolean validIni;
-	private Button submitCreateOperator;
 	
 	public CreateWidget(DataServiceAsync service) {
-
-		this.serverConn = service;
-
-		// create the verticalPanel to hold this view.
-		view = new VerticalPanel();
-		initWidget(this.view);
-
-		// Create FlexTable to hold the labels and text box's
-		FlexTable table = new FlexTable();
-		table.setStyleName("create-operator");
-		
-		// Insert label and textbox for Opr_name
-		Label oprNameLabel = new Label("Operator name:");
-		oprNameTextBox = new TextBox();
-		table.setWidget(0, 0, oprNameLabel);
-		table.setWidget(0, 1, oprNameTextBox);
-
-		// Insert label and textbox for Ini
-		Label iniLabel = new Label("Ini:");
-		iniTextBox = new TextBox();
-		table.setWidget(1, 0, iniLabel);
-		table.setWidget(1, 1, iniTextBox);
-
-		// Insert label and textbox for cpr
-		Label cprLabel = new Label("CPR-number:");
-		cprTextBox = new TextBox();
-		table.setWidget(2, 0, cprLabel);
-		table.setWidget(2, 1, cprTextBox);
-
-		// create the submit button
-		submitCreateOperator = new Button("Create Operator");
-		submitCreateOperator.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				OperatorDTO newOperator = new OperatorDTO(0, oprNameTextBox.getText(), iniTextBox.getText(), cprTextBox.getText(), "");
-				serverConn.createOperator(newOperator, new AsyncCallback<OperatorDTO>() {
-
-					@Override
-					public void onFailure(Throwable caught) {
-						
-				
-						
-						if (caught instanceof DALException) {
-							Window.alert("Kunne ikke forbinde til databasen.");
-						}
-						
-						else {
-							Window.alert("I give up");
-						}
-						
-
-					}
-
-					@Override
-					public void onSuccess(OperatorDTO newOperator) {
-						Window.alert("Operatør oprettet med ID: " + newOperator.getOprID() + " og password: " + newOperator.getPassword());
-						oprNameTextBox.setText("");
-						cprTextBox.setText("");
-						iniTextBox.setText("");
-
-					}
-
-				});
-			}
-		});
-
-		oprNameTextBox.addKeyUpHandler(new KeyUpHandler(){
-			@Override
-			public void onKeyUp(KeyUpEvent event) {
-				if(FieldVerifier.isValidName(oprNameTextBox.getText())){
-					oprNameTextBox.removeStyleName("invalidEntry");
-					validName = true;
-				} else{
-					oprNameTextBox.setStyleName("invalidEntry");
-					validName = false;
-				}
-				checkForm();
-			}
-		});
-		
-		
-		iniTextBox.addKeyUpHandler(new KeyUpHandler(){
-			@Override
-			public void onKeyUp(KeyUpEvent event) {
-				if(FieldVerifier.isValidInitials(iniTextBox.getText())){
-					iniTextBox.removeStyleName("invalidEntry");
-					validIni = true;
-				} else{
-					iniTextBox.setStyleName("invalidEntry");
-					validIni = false;
-				}
-				checkForm();
-			}
-		});
-		
-		cprTextBox.addKeyUpHandler(new KeyUpHandler(){
-			@Override
-			public void onKeyUp(KeyUpEvent event) {
-				if(FieldVerifier.isValidCPR(cprTextBox.getText())){
-					cprTextBox.removeStyleName("invalidEntry");
-					validCPR = true;
-				} else{
-					cprTextBox.setStyleName("invalidEntry");
-					validCPR = false;
-				}
-				checkForm();
-			}
-		});
-		
-		table.setWidget(3, 0, submitCreateOperator);
-		view.add(table);
+		initWidget(uiBinder.createAndBindUi(this));
+		this.service = service;
 	}
 	
+	@UiHandler("submitCreateOperator")
+	void clickSubmit(ClickEvent e) {
+		OperatorDTO newOperator = new OperatorDTO(0, oprNameTextBox.getText(), iniTextBox.getText(), cprTextBox.getText(), "");
+		service.createOperator(newOperator, new AsyncCallback<OperatorDTO>() {
+
+			@Override
+			public void onFailure(Throwable caught) {
+				if (caught instanceof DALException) {
+					Window.alert("Kunne ikke forbinde til databasen.");
+				}
+				else {
+					Window.alert("I give up");
+				}
+			}
+
+			@Override
+			public void onSuccess(OperatorDTO newOperator) {
+				Window.alert("Operatør oprettet med ID: " + newOperator.getOprID() + " og password: " + newOperator.getPassword());
+				oprNameTextBox.setText("");
+				cprTextBox.setText("");
+				iniTextBox.setText("");
+
+			}
+
+		});
+	}
+	
+	@UiHandler("oprNameTextBox")
+	void keyUpName(KeyUpEvent e) {
+		if(FieldVerifier.isValidName(oprNameTextBox.getText())){
+			oprNameTextBox.removeStyleName("invalidEntry");
+			validName = true;
+		} else{
+			oprNameTextBox.setStyleName("invalidEntry");
+			validName = false;
+		}
+		checkForm();
+	}
+
+	@UiHandler("iniTextBox")
+	void keyUpIni(KeyUpEvent e) {
+		if(FieldVerifier.isValidInitials(iniTextBox.getText())){
+			iniTextBox.removeStyleName("invalidEntry");
+			validIni = true;
+		} else{
+			iniTextBox.setStyleName("invalidEntry");
+			validIni = false;
+		}
+		checkForm();
+	}
+	
+	@UiHandler("cprTextBox")
+	void keyUpCpr(KeyUpEvent e) {
+		if(FieldVerifier.isValidCPR(cprTextBox.getText())){
+			cprTextBox.removeStyleName("invalidEntry");
+			validCPR = true;
+		} else{
+			cprTextBox.setStyleName("invalidEntry");
+			validCPR = false;
+		}
+		checkForm();
+	}
+
 	private void checkForm(){
 		if (validName && validCPR && validIni){
 			submitCreateOperator.setEnabled(true);
@@ -148,4 +110,5 @@ public class CreateWidget extends Composite {
 			submitCreateOperator.setEnabled(false);
 		}
 	}
+	
 }
