f56d8fb0767eab0f096f827728ff62da309a275c
al <a@saloweb.dk>
2015-05-09 16:54:22 +0200

    operators page is now uiBinder

--- src/com/cdio3/client/boundary/views/admin/OperatorsWidget.java
+++ src/com/cdio3/client/boundary/views/admin/OperatorsWidget.java
@@ -3,12 +3,16 @@ package com.cdio3.client.boundary.views.admin;
 import java.util.List;
 
 import com.cdio3.client.service.DataServiceAsync;
+import com.cdio3.shared.FieldVerifier;
 import com.cdio3.shared.OperatorDTO;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Document;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Anchor;
@@ -17,22 +21,29 @@ import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.TextBox;
-import com.cdio3.shared.FieldVerifier;
-import com.google.gwt.dom.client.Document;
+import com.google.gwt.user.client.ui.Widget;
 public class OperatorsWidget extends Composite {
-	private FlexTable table;
-	private int editRow;
-	private Button cancelButton;
+	
+	interface OperatorsUiBinder extends UiBinder<Widget, OperatorsWidget> {
+	}
+
+	private static OperatorsUiBinder uiBinder = GWT.create(OperatorsUiBinder.class);
+	
 	private DataServiceAsync service;
+	
+	@UiField FlexTable table;
+	
+	private int editRow = -1;
+	
 	private boolean validName;
 	private boolean validCPR;
 	private boolean validIni;
 	private boolean validPassword;
-	private Button currentSubmitButton;
+	
 	public OperatorsWidget(DataServiceAsync service) {
+		initWidget(uiBinder.createAndBindUi(this));
 		this.service = service;
-		table = new FlexTable();
-		initWidget(table);
+
 		table.setStyleName("operators");
 		service.getAllOperators(new getOperatorsCallback());
 
@@ -72,165 +83,58 @@ public class OperatorsWidget extends Composite {
 
 	}
 
+	private Label oprID = new Label();
+	@UiField TextBox oprName;
+	@UiField TextBox oprIni;
+	@UiField TextBox oprCPR;
+	@UiField TextBox oprPassword;
+	@UiField Button submitButton;
+	@UiField Button cancelButton;
+	
 	private class EditClickHandler implements ClickHandler {
 
+		private String getTableLabelText(int column){
+			return ((Label) table.getWidget(editRow+1, column)).getText();
+		}
+		
 		@Override
 		public void onClick(ClickEvent event) {
-
-			if (cancelButton != null) {
-				cancelButton.fireEvent(new ClickEvent() {
-				});
+			
+			if (editRow > -1) {
+				cancelButton.fireEvent(new ClickEvent() {});
 			}
 
 			editRow = table.getCellForEvent(event).getRowIndex();
+			table.insertRow(editRow);
+			
+			table.getRowFormatter().setVisible(editRow+1, false);
+			
+			oprID.setText(getTableLabelText(0));
+			table.setWidget(editRow, 0, oprID);
+						
+			oprName.setText(getTableLabelText(1));
+			table.setWidget(editRow, 1, oprName);
+			
+			oprIni.setText(getTableLabelText(2));
+			table.setWidget(editRow, 2, oprIni);
+			
+			oprCPR.setText(getTableLabelText(3));
+			table.setWidget(editRow, 3, oprCPR);
+			
+			oprPassword.setText(getTableLabelText(4));
+			table.setWidget(editRow, 4, oprPassword);
 
-			for (int i = 1; i < 5; i++) {
-				TextBox editTextBox = new TextBox();
-				Label oldLabel = (Label) table.getWidget(editRow, i);
-				editTextBox.setText(oldLabel.getText());
-				table.clearCell(editRow, i);
-				table.setWidget(editRow, i, editTextBox);
-			}
-			
-			final TextBox oprNameTextBox = (TextBox) table.getWidget(editRow, 1);
-			oprNameTextBox.addKeyUpHandler(new KeyUpHandler(){
-				@Override
-				public void onKeyUp(KeyUpEvent event) {
-					if(FieldVerifier.isValidName(oprNameTextBox.getText())){
-						oprNameTextBox.removeStyleName("invalidEntry");
-						validName = true;
-					} else{
-						oprNameTextBox.setStyleName("invalidEntry");
-						validName = false;
-					}
-					checkForm();
-				}
-			});
-			
-			
-			final TextBox oprIniTextBox = (TextBox) table.getWidget(editRow, 2);
-			oprIniTextBox.addKeyUpHandler(new KeyUpHandler(){
-				@Override
-				public void onKeyUp(KeyUpEvent event) {
-					if(FieldVerifier.isValidInitials(oprIniTextBox.getText())){
-						oprIniTextBox.removeStyleName("invalidEntry");
-						validIni = true;
-					} else{
-						oprIniTextBox.setStyleName("invalidEntry");
-						validIni = false;
-					}
-					checkForm();
-				}
-				
-			});
-			
-			final TextBox oprCPRTextBox = (TextBox) table.getWidget(editRow, 3);
-			oprCPRTextBox.addKeyUpHandler(new KeyUpHandler(){
-				@Override
-				public void onKeyUp(KeyUpEvent event) {
-					if(FieldVerifier.isValidCPR(oprCPRTextBox.getText())){
-						oprCPRTextBox.removeStyleName("invalidEntry");
-						validCPR = true;
-					} else{
-						oprCPRTextBox.setStyleName("invalidEntry");
-						validCPR = false;
-					}
-					checkForm();
-				}
-				
-			});
-			
-			final TextBox oprPasswordTextBox = (TextBox) table.getWidget(editRow, 4);
-			oprPasswordTextBox.addKeyUpHandler(new KeyUpHandler(){
-				@Override
-				public void onKeyUp(KeyUpEvent event) {
-					if(FieldVerifier.isValidPassword(oprPasswordTextBox.getText())){
-						oprPasswordTextBox.removeStyleName("invalidEntry");
-						validPassword = true;
-					} else{
-						oprPasswordTextBox.setStyleName("invalidEntry");
-						validPassword = false;
-					}
-					checkForm();
-				}
-			});
-			
-			
-			
-			Button submitButton = new Button("Submit");
-			currentSubmitButton = submitButton;
-			submitButton.addClickHandler(new SubmitClick());
-			checkForm();
-			table.clearCell(editRow, 5);
 			table.setWidget(editRow, 5, submitButton);
-
-			table.setWidget(editRow, 7, table.getWidget(editRow, 6));
-			
-			cancelButton = new Button("Cancel");
-			cancelButton.addClickHandler(new CancelClick());
 			table.setWidget(editRow, 6, cancelButton);
-			
-			KeyUpEvent.fireNativeEvent(Document.get().createKeyUpEvent(false, false, false, false, 65), oprNameTextBox);
-			KeyUpEvent.fireNativeEvent(Document.get().createKeyUpEvent(false, false, false, false, 65), oprIniTextBox);
-			KeyUpEvent.fireNativeEvent(Document.get().createKeyUpEvent(false, false, false, false, 65), oprCPRTextBox);
-			KeyUpEvent.fireNativeEvent(Document.get().createKeyUpEvent(false, false, false, false, 65), oprPasswordTextBox);
+						
+			KeyUpEvent.fireNativeEvent(Document.get().createKeyUpEvent(false, false, false, false, 65), oprName);
+			KeyUpEvent.fireNativeEvent(Document.get().createKeyUpEvent(false, false, false, false, 65), oprIni);
+			KeyUpEvent.fireNativeEvent(Document.get().createKeyUpEvent(false, false, false, false, 65), oprCPR);
+			KeyUpEvent.fireNativeEvent(Document.get().createKeyUpEvent(false, false, false, false, 65), oprPassword);
 
 		}
 	}
 	
-	private class SubmitClick implements ClickHandler {
-
-		@Override
-		public void onClick(ClickEvent event) {
-			int id = Integer.parseInt(((Label) table.getWidget(editRow, 0)).getText());
-			String name = ((TextBox) table.getWidget(editRow, 1)).getText();
-			String ini = ((TextBox) table.getWidget(editRow, 2)).getText();
-			String cpr = ((TextBox) table.getWidget(editRow, 3)).getText();
-			String password = ((TextBox) table.getWidget(editRow, 4)).getText();
-
-			OperatorDTO operator = new OperatorDTO(id, name, ini, cpr, password);
-			service.updateOperator(operator, new AsyncCallback<Void>() {
-
-				@Override
-				public void onFailure(Throwable caught) {
-					// TODO Auto-generated method stub
-					Window.alert("Operatøren blev IKKE oprettet, der kunne ikke komme forbindelse til din database");
-				}
-
-				@Override
-				public void onSuccess(Void result) {
-					Window.alert("Operatøren er blevet opdateret");
-					cancelButton.fireEvent(new ClickEvent() {
-					});
-				}
-
-			});
-		}
-
-	}
-
-	private class CancelClick implements ClickHandler {
-
-		@Override
-		public void onClick(ClickEvent event) {
-			for (int i = 1; i < 5; i++) {
-				Label newLabel = new Label();
-				TextBox oldTextBox = (TextBox) table.getWidget(editRow, i);
-				newLabel.setText(oldTextBox.getText());
-				table.clearCell(editRow, i);
-				table.setWidget(editRow, i, newLabel);
-			}
-			table.clearCell(editRow, 5);
-			Anchor editAnchor = new Anchor("Edit");
-			editAnchor.addClickHandler(new EditClickHandler());
-			table.setWidget(editRow, 5, editAnchor);
-			table.setWidget(editRow, 6, table.getWidget(editRow, 7));
-			table.clearCell(editRow, 7);
-			cancelButton = null;
-		}
-
-	}
-
 	private class deleteClickHandler implements ClickHandler{
 		private int deleteRow;
 
@@ -256,14 +160,103 @@ public class OperatorsWidget extends Composite {
 			}
 		}
 		
-	}
+	}	
 	
 	private void checkForm(){
 		if(validName && validIni && validCPR && validPassword){
-			currentSubmitButton.setEnabled(true);
+			submitButton.setEnabled(true);
 		} else{
-			currentSubmitButton.setEnabled(false);
+			submitButton.setEnabled(false);
 		}
+	}
+	
+	@UiHandler("oprName")
+	void keyUpName(KeyUpEvent event) {
+		if(FieldVerifier.isValidName(oprName.getText())){
+			oprName.removeStyleName("invalidEntry");
+			validName = true;
+		} else{
+			oprName.setStyleName("invalidEntry");
+			validName = false;
+		}
+		checkForm();
+	}
+	
+	@UiHandler("oprIni")
+	void keyUpIni(KeyUpEvent event) {
+		if(FieldVerifier.isValidInitials(oprIni.getText())){
+			oprIni.removeStyleName("invalidEntry");
+			validIni = true;
+		} else{
+			oprIni.setStyleName("invalidEntry");
+			validIni = false;
+		}
+		checkForm();
+	}
+	
+	@UiHandler("oprCPR")
+	void keyUpCPR(KeyUpEvent event) {
+		if(FieldVerifier.isValidCPR(oprCPR.getText())){
+			oprCPR.removeStyleName("invalidEntry");
+			validCPR = true;
+		} else{
+			oprCPR.setStyleName("invalidEntry");
+			validCPR = false;
+		}
+		checkForm();
+	}
+	
+	@UiHandler("oprPassword")
+	void keyUpPassword(KeyUpEvent event) {
+		if(FieldVerifier.isValidPassword(oprPassword.getText())){
+			oprPassword.removeStyleName("invalidEntry");
+			validPassword = true;
+		} else{
+			oprPassword.setStyleName("invalidEntry");
+			validPassword = false;
+		}
+		checkForm();
+	}
+	
+	@UiHandler("submitButton")
+	void clickSubmit(ClickEvent event) {
+		int id = Integer.parseInt(((Label) table.getWidget(editRow, 0)).getText());
+		String name = ((TextBox) table.getWidget(editRow, 1)).getText();
+		String ini = ((TextBox) table.getWidget(editRow, 2)).getText();
+		String cpr = ((TextBox) table.getWidget(editRow, 3)).getText();
+		String password = ((TextBox) table.getWidget(editRow, 4)).getText();
+
+		OperatorDTO operator = new OperatorDTO(id, name, ini, cpr, password);
+		service.updateOperator(operator, new AsyncCallback<Void>() {
+
+			@Override
+			public void onFailure(Throwable caught) {
+				// TODO Auto-generated method stub
+				Window.alert("Operatøren blev IKKE oprettet, der kunne ikke komme forbindelse til din database");
+			}
+
+			@Override
+			public void onSuccess(Void result) {
+				Window.alert("Operatøren er blevet opdateret");
+				
+				((Label) table.getWidget(editRow+1, 1)).setText(oprName.getText());
+				((Label) table.getWidget(editRow+1, 2)).setText(oprIni.getText());
+				((Label) table.getWidget(editRow+1, 3)).setText(oprCPR.getText());
+				((Label) table.getWidget(editRow+1, 4)).setText(oprPassword.getText());
+				
+				cancelButton.fireEvent(new ClickEvent() {});
+			}
+
+		});
+	}
+
+	@UiHandler("cancelButton")
+	void clickCancel(ClickEvent event) {
+				
+		table.getRowFormatter().setVisible(editRow+1, true);
+		table.getRowFormatter().setVisible(editRow, false);
+
+		editRow = -1;
 	}
 	
 }
