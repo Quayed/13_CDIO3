6336bad471440bb3e4227ebb6e81c1596b6bee73
al <a@saloweb.dk>
2015-05-10 21:02:01 +0200

    sql file updated

--- database.sql
+++ database.sql
@@ -12,3 +12,5 @@ INSERT INTO operator(opr_id, opr_name, ini, cpr, password) VALUES
 (1, 'Angelo A', 'AA', '0707707007', 'lKje4fa'),
 (2, 'Antonella B', 'AB', '0808808008', 'atoJ21v'),
 (3, 'Luigi C', 'LC', '0909909009', 'jEfm5aQ');
+
+ALTER TABLE tbl AUTO_INCREMENT = 11;
